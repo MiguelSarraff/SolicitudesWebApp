cd96ae0220a2b419f7d87499bfa1cbbdda5d9f55
Fernandoegarcia <fernandoegarcia25@gmail.com>
2024-05-23 19:00:05 -0400

    solicitud_cupo

--- SolicitudesAppFrontend/src/app/app.module.ts
+++ SolicitudesAppFrontend/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UsuarioModule } from './usuario/usuario.module';
+import { CuposComponent } from './cupos/cupos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CuposComponent
   ],
   imports: [
     BrowserModule,
